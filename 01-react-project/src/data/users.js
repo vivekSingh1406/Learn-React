1b3a594d8bc68e268652270c94864b5f6fea392d
viveksingh1406 <viveksb1207@gmail.com>
2025-07-02 12:30:30 +0530

    updates users data

--- 01-react-project/src/data/users.js
+++ 01-react-project/src/data/users.js
@@ -1,7 +1,8 @@
 export const users = [
   { name: 'vivek singh', district: 'Shahdol', country: 'India' },
   { name: 'vikash singh', district: 'Shahdol', country: 'India' },
-  { name: 'Dipeendra', district: 'Satna', country: 'india' },
+  { name: 'ravi singh', district: 'Rewa', country: 'India'},
+  { name: 'Dipeendra', district: 'Satna', country: 'India' },
   { name: 'Liu Wei', district: 'Shanghai', country: 'China' },
   { name: 'Sarah Kim', district: 'Seoul', country: 'South Korea' }
 ];
